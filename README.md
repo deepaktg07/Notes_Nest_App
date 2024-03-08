7d57a21d8b5485753761612342ed9127cf2fb3c8
Deepak Gonchikar <deepakgonchikar0707@gmail.com>
2024-03-08 09:36:34 +0530

    notes nest app

---
+++ README.md
@@ -0,0 +1,112 @@
+# NoteNest
+
+NoteNest is a minimalist note-taking app that enhances productivity and organization with its clean design and efficient functionality.
+
+## Table of Contents
+
+- [NoteNest](#notenest)
+  - [Table of Contents](#table-of-contents)
+  - [Features](#features)
+  - [Technologies](#technologies)
+    - [Client Side](#client-side)
+    - [Server Side](#server-side)
+  - [Getting Started](#getting-started)
+    - [Client Setup](#client-setup)
+    - [Server Setup](#server-setup)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+
+## Features
+
+- Create, edit, and delete notes
+- Clean and intuitive user interface
+- Real-time updates with server integration
+
+## Technologies
+
+### Client Side
+
+- React
+- Tailwind CSS
+
+### Server Side
+
+- Node.js
+- Express.js
+- MongoDB with Mongoose for data storage
+
+## Getting Started
+
+### Client Setup
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/khatri-viren/NoteNest.git
+   ```
+
+2. Navigate to the client directory:
+
+   ```bash
+   cd NoteNest/client
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Start the development server:
+
+   ```bash
+   npm run dev
+   ```
+
+5. Open your browser and visit `http://localhost:3000`.
+
+### Server Setup
+
+1. Navigate to the server directory:
+
+   ```bash
+   cd NoteNest/server
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+3. Configure the MongoDB connection:
+
+   - Create a `.env` file in the `server` directory.
+   - Add your MongoDB connection string:
+
+     ```env
+     MONGODB_URI=your-mongodb-uri
+     ```
+
+4. Start the server:
+
+   ```bash
+   npm run dev
+   ```
+
+5. The server will run at `http://localhost:4000`.
+
+## Usage
+
+- Open the app in your browser.
+- Create a new note, edit existing notes, or delete them.
+- Enjoy a simplified and efficient note-taking experience!
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues and pull requests.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ server/index.js
@@ -0,0 +1,111 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const configDotenv = require("dotenv");
+configDotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+mongoose.connect(`${process.env.MONGODB_URI}`).then(
+  () => {
+    console.log("Connnected to Mongoose successfully!");
+  },
+  (err) => {
+    console.log("Error while connecting", err);
+  }
+);
+
+// Define a Note schema
+const noteSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ); // Add 'Authorization' to the header
+  next();
+});
+
+app.use(cors());
+
+// Routes
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "NoteNest Server is Live" });
+});
+
+app.get("/api/notes", async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  // console.log(req.body);
+  try {
+    const newNote = new Note({ title, content });
+    // console.log(newNote);
+    await newNote.save();
+    res.json(newNote);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    // Check if the note with the given ID exists
+    const existingNote = await Note.findById(id);
+
+    if (!existingNote) {
+      return res.status(404).json({ error: "Note not found" });
+    }
+
+    // Update the note
+    const updatedNote = await Note.findByIdAndUpdate(
+      id,
+      { title, content },
+      { new: true }
+    );
+
+    res.json(updatedNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Note.findByIdAndDelete(id);
+    res.json({ message: "Note deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
